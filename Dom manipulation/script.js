0d80e3bfc354854835c28f0c44757ee90abb3359
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-07-06 14:04:56 +0200

    Add h3 styling

--- Dom manipulation/script.js
+++ Dom manipulation/script.js
@@ -5,3 +5,19 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const para = document.createElement('p');
+para.classList.add('para');
+para.textContent = "Hey I'm red!";
+para.style.color = "red";
+
+container.appendChild(para);
+
+const header = document.createElement("h3");
+header.classList.add("h3");
+header.textContent = "I'm a blue h3!";
+header.style.color = "blue";
+
+container.appendChild(header);
+
+
